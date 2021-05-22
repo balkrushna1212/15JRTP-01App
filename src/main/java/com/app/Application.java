21f980303ca8db690b7338a9a680539572008d7f
Balkrishna <balkrishnabhojagude@gmail.com>
2021-05-22 16:57:04 +0530

    HIS-101 changes done

--- src/main/java/com/app/Application.java
+++ src/main/java/com/app/Application.java
@@ -1,11 +1,11 @@
 package com.app;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//HIS-101
 @SpringBootApplication
 public class Application {
-
+	private static String URL-ReST="http://abc.com";
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
